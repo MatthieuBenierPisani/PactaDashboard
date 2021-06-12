f24e01c018fe807354a396464df9ee4547ed2256
bellottoeric <bellottoeric@gmail.com>
2021-06-12 10:29:03 +0200

    Fetch articles and send them to /askArticles

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+var scrapArticles = require('./scrapArticles.js').scrapArticles
+
 
 app.use(express.static('www'));
 
@@ -10,4 +12,10 @@ var server = app.listen(8000, function () {
 
     console.log('Express app listening at http://%s:%s', host, port)
 
+})
+scrapArticles()
+app.get('/askArticles', async function (req, res) {
+    var articles = await scrapArticles()
+    console.log(articles)
+    res.send(articles)
 })
---
+++ scrapArticles.js
@@ -0,0 +1,45 @@
+const got = require('got');
+const cheerio = require('cheerio');
+
+var urlToScrap = "https://2degrees-investing.org/resources"
+
+async function scrapArticles() {
+    return (new Promise(async (resolve, reject) => {
+        try {
+            const response = await got(urlToScrap);
+            const $ = cheerio.load(response.body);
+            var fullElem = []
+            $('div[class="work-item"]').find('div').each(function (index, element) {
+                var elem = {}
+                if (element.children[0].children && element.children[0].children[1]) {
+                    //console.log(element.children[0].children[1].children[0].data) // title
+                    //console.log(element.children[0].children[3].children[0].data) // description
+                    if (element.children[0].children[1].children[0].data.indexOf('screen and') === -1) {
+                        elem.title = element.children[0].children[1].children[0].data
+                        elem.description = element.children[0].children[3].children[0].data
+                    }
+                }
+                if (element.children[1]) {
+                    for (var i of element.children[1].children) {
+                        var tag = $(i).text().replace(/\n/g, "").replace(/       /g, "")
+                        if (tag.length > 5 && tag.indexOf('\t') === -1) {
+                            if (!elem.tags)
+                                elem.tags = []
+                            elem.tags.push(tag.replace('   ', ''))
+                        }
+                    }
+                }
+                if (elem.title)
+                    fullElem.push(elem)
+                //console.log()
+            });
+            console.log(fullElem)
+            resolve(fullElem)
+        } catch (e) {
+            console.log('Error in function', arguments.callee.name, e)
+            resolve('2')
+        }
+    }))
+}
+
+exports.scrapArticles = scrapArticles
