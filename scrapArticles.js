a94d406e16505a0fdcf6a6e28869754118351bad
MatthieuBenierPisani <matthieu.benier-pisani@epitech.eu>
2021-06-14 16:03:21 +0200

    Putain de children

--- scrapArticles.js
+++ scrapArticles.js
@@ -72,6 +72,81 @@ async function bpce() {
     }))
 }
 
+async function bbva() {
+    return (new Promise(async (resolve, reject) => {
+        try {
+            const response = await got("https://search.bbva.com/en/bbva?searchbbvaen=katowice");
+            const $ = cheerio.load(response.body);
+
+            $('.resource').each(function (index, element) {
+                console.log(element);
+                console.log('here');
+    
+                if (element.children[2]) {
+                    var elem = {}
+                    var link = element.children
+                    if (element.attribs.title) {
+                        elem.description = element.attribs.title
+                    } else {
+                        elem.description = element.attribs["data-tracking-click-libelle"]
+                    }
+                    /*
+                    if (link.indexOf('\/\/') !== -1) {
+                        elem.url = "https:" + element.attribs.href
+                    } else {
+                        elem.url = "https://groupebpce.com" + element.attribs.href
+                    }
+                    */
+                    elem.title = $(element.children[3].children[1]).text().trim().replace(/\t/g, " ").replace(/  /g, " ").replace(/  /g, " ").replace(/  /g, " ").replace(/  /g, " ").replace(/\n/g, " ")
+                    fullElem.push(elem)
+                }
+            })
+            resolve()
+        } catch (e) {
+            console.log('Error in function', arguments.callee.name, e)
+        }
+    }))
+}
+
+async function societegenerale() {
+    return (new Promise(async (resolve, reject) => {
+        try {
+            const response = await got("https://www.societegenerale.com/fr/communiques-de-presse");
+            const $ = cheerio.load(response.body);
+
+            $('.node--type-communique-presse').each(function (index, element) {
+                if (element.children[3]) {
+                    var elem = {}
+                    var link = element.children[2].attribs.href
+                    console.log(link);
+                }
+                /*
+                if (element.children[0]) {
+                    var elem = {}
+                    var link = element.attribs.href
+                    if (element.attribs.title) {
+                        elem.description = element.attribs.title
+                    } else {
+                        elem.description = element.attribs["data-tracking-click-libelle"]
+                    }
+                    if (link.indexOf('\/\/') !== -1) {
+                        elem.url = "https:" + element.attribs.href
+                    } else {
+                        elem.url = "https://groupebpce.com" + element.attribs.href
+                    }
+                    elem.title = $(element.children[3].children[1]).text().trim().replace(/\t/g, " ").replace(/  /g, " ").replace(/  /g, " ").replace(/  /g, " ").replace(/  /g, " ").replace(/\n/g, " ")
+                    fullElem.push(elem)
+                }
+                */
+            })
+            resolve()
+        } catch (e) {
+            console.log('Error in function', arguments.callee.name, e)
+        }
+    }))
+}
+
+
 async function bnp() {
     return (new Promise(async (resolve, reject) => {
         try {
@@ -118,6 +193,28 @@ async function sc() {
     }))
 }
 
+async function newsroom() {
+    return (new Promise(async (resolve, reject) => {
+        try {
+
+            let feed = await parser.parseURL('https://newsroom.ing.fr/feed/');
+            console.log(feed.title);
+
+            feed.items.forEach(item => {
+                var elem = {}
+                if (item.link) {
+                    elem.title = "NEWSROOM ING"
+                    elem.description = item.title
+                    elem.url = item.link
+                    fullElem.push(elem)
+                }
+            });
+            resolve()
+        } catch (e) {
+            console.log('Error in function', arguments.callee.name, e)
+        }
+    }))
+}
 
 async function scrapArticles(e) {
     return (new Promise(async (resolve, reject) => {
@@ -125,7 +222,10 @@ async function scrapArticles(e) {
             //await degrees()
             //await bnp()
             //await bpce()
+            //await bbva()
+            await societegenerale()
             //await sc()
+            //await newsroom()
             console.log(fullElem.length)
             resolve(fullElem)
         } catch (e) {
